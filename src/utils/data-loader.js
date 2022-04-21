26cc7d9d223df16d68170d2daee13347a6487fcd
Per Rawdin <pr222iw@student.lnu.se>
2022-04-21 17:57:39 +0200

    Add data fetch invocation

--- src/utils/data-loader.js
+++ src/utils/data-loader.js
@@ -1,6 +1,6 @@
 /**
  * Data-loader module.
- * Fetches data from API and ingest into elasticsearch.
+ * Script that fetches data from API and ingest into elasticsearch.
  *
  * @author Per Rawdin <per.rawdin@student.lnu.se>
  * @version 1.0.0
@@ -32,4 +32,51 @@ const client = new Client({
 async function fetchData (url) {
   const response = await axios(url)
   return await response.json()
+}
+
+/**
+ * Variables for API calls.
+ */
+const languages = ['javascript', 'html css', 'python', 'typescript', 'node js', 'bash', 'php', 'c%23', 'sql', 'java']
+const years = ['2016', '2017', '2018', '2019', '2020', '2021']
+const quarterStart = ['-01-01', '-04-01', '-07-01', '-10-01']
+const quarterEnd = ['-03-31', '-06-30', '-09-30', '-12-31']
+
+/**
+ * Make API calls.
+ */
+// Loop each language.
+for (const language of languages) {
+  // Loop each year.
+  for (const year of years) {
+    // Loop each quarter.
+    for (let quarter = 0; quarter < quarterEnd.length; quarter++) {
+      // Data container.
+      const data = []
+
+      // Variables for API pagination.
+      let pages = true
+      let count = 0
+      let offset = 0
+
+      while (pages) {
+        // Fetch data.
+        const url = `${process.env.API_URL}/search?q=${language}&historical-from=${year}${quarterStart[quarter]}&historical-to=${year}${quarterEnd[quarter]}&limit=100&offset=${offset}&resdet=brief`
+        const response = await fetchData(url)
+
+        data.push(...response.hits)
+
+        // Increment count for next page.
+        count++
+        offset = (count * 100) + 1
+
+        // If page contains less than 100 results, stop current iteration.
+        if (response.hits.length < 100) pages = false
+      }
+
+      // Add language tag for each document.
+      data.forEach(document => (document.tag = language))
+    }
+    console.log(`Year: ${year} completed`)
+  }
 }
