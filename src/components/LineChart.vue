56411a99e0f57c7519475a55655c7a787163fece
Per Rawdin <pr222iw@student.lnu.se>
2022-04-22 11:39:06 +0200

    Add LineChart component and template

---
+++ src/components/LineChart.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class='linechart'>
+    <apexcharts type='line' :options='chartOptions' :series='series'></apexcharts>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'LineChart'
+}
+</script>
