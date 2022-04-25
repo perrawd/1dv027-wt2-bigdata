683dea9432de6f28ed1b001f9f1b889f9cd4d529
Per Rawdin <pr222iw@student.lnu.se>
2022-04-25 18:27:07 +0200

    Add comments in LineChart component

--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -1,13 +1,23 @@
 <template>
+  <!-- Display chart if data has been loaded sucessfully -->
   <div v-if="series" class='linechart'>
     <apexcharts type='line' :options='chartOptions' :series='series'/>
   </div>
+  <!-- Else display loading spinner. -->
   <div v-else class="d-flex justify-content-center mb-3 loading">
     <b-spinner/>
   </div>
 </template>
 
 <script>
+/**
+ *
+ * LineChart component.
+ * Fetches data from Elasticsearch and presents it in a linechart.
+ *
+ * @author Per Rawdin <per.rawdin@student.lnu.se>
+ * @version 1.0.0
+ */
 import VueApexCharts from 'vue-apexcharts'
 import axios from 'axios'
 import { countAllYears } from '../queries/count_all_years.js'
@@ -18,14 +28,17 @@ export default {
     apexcharts: VueApexCharts
   },
   mounted () {
+    // Fetch data from Elasticsearch.
     axios({
       method: 'post',
       url: process.env.VUE_APP_URL,
       headers: {
         Authorization: process.env.VUE_APP_AUTH
       },
+      // Query
       data: countAllYears
     })
+      // Map response data to object variable.
       .then(response => (this.series = response.data.aggregations.langs.buckets.map(
         language => (
           {
@@ -38,6 +51,7 @@ export default {
   },
   data () {
     return {
+      // Chart configuration.
       chartOptions: {
         stroke: {
           curve: 'smooth'
